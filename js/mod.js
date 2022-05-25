9ae0401247b05cf6030bf79bcc1d77cc9917ed39
3xtremeLamborghini <ariescestr215@gmail.com>
2022-05-25 12:51:38 -0400

    Update mod.js
    Changed the name of the mod, changed the id, and changed the author

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The testing Tree",
+	id: "215081106",
+	author: "3xtremeLamborghini",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
